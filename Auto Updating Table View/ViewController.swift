cc6d5327b79204c5c35332532a6a3e90cf4a2ade
Nathan Fennel <nathanfennel@gmail.com>
2020-08-25 22:23:12 -0600

    Initial Commit

---
+++ Auto Updating Table View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Updating Table View
+//
+//  Created by Nathan Fennel on 8/25/20.
+//  Copyright © 2020 Nathan Fennel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
